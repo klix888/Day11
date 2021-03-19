0753e9302b32099ea27c43d872e63728df5b7254
Dzevad <ynwa886@gmail.com>
2021-03-19 02:30:04 +0100

    Splice

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+//Only change code below this line
+function mySplice(arr1, arr2, n) {
+    
+    arr2.splice(n, 0, ...arr1.reverse());
+
+
+
+return arr2;
+}
+
+
+//Only change code below this line
+
+console.log(mySplice([1, 2, 3], [4, 5], 1));
+console.log(mySplice([1, 2, 3], [4, 5], 2));
+console.log(mySplice(["b", "c"], ["a", "d"], 1));
+console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2));
+module.exports = mySplice;
